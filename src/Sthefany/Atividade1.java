411ba3199f2252e0e55324ad98b8ce726b4c18aa
Sthefany Silva <sthefany0011@gmail.com>
2020-11-12 23:22:38 -0300

    first commit

---
+++ src/Sthefany/Atividade1.java
@@ -0,0 +1,18 @@
+package Sthefany;
+
+public class Atividade1 {
+
+ public static void main(String[] args) {
+	// TODO Auto-generated method stub
+System.out.println("\nSou Sthefany Silva, tenho 26 anos, moro em Monte Mor/SP."
+	+ "\nAtualmente sou estudante na Generation no curso de Desenvolvedor Java Jr. Já "
+	+ "estamos na terceira semana do curso."
+	+ "\n O curso abordou temas comportamental e aplicação da aritmética,Como usar o \n"
+	+ "Portugol, vetores, matrizes, laço de repetição e finalmente "
+	+ "estamos entrando no conceito Java.\n"
+	+ "\nGosto de viajar,de ouvir musica, estudar, curtir momentos familias e amigos, \namo chocolate e "
+	+ "por ai vai. ");
+}
+
+}
+
